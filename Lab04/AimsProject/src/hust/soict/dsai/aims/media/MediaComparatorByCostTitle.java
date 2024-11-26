2b9cb6103644bcaf9dc5cbfaf5fa82402679cdfa
tranbinhminhsoict <tuilaminh28@gmail.com>
2024-11-26 14:35:11 +0700

    Add files via upload

---
+++ Lab04/AimsProject/src/hust/soict/dsai/aims/media/MediaComparatorByCostTitle.java
@@ -0,0 +1,10 @@
+package hust.soict.dsai.aims.media;
+import java.util.Comparator;
+
+public class MediaComparatorByCostTitle implements Comparator<Media> {
+	public int compare(Media media1, Media media2) {
+        return Comparator.comparing(Media::getCost)
+                .thenComparing(Media::getTitle)
+                .compare(media1, media2);
+    }
+}
